c4c9f1265ce1c94cd8cb2e6eadefc91bca06a61a
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2024-08-31 15:34:11 -0300

    Adicionando método de busca em largura no Grafo.

--- grafos/src/main/java/br/com/louzeiroheitor/classes/Grafo.java
+++ grafos/src/main/java/br/com/louzeiroheitor/classes/Grafo.java
@@ -3,26 +3,30 @@ package br.com.louzeiroheitor.classes;
 import java.util.*;
 
 class Grafo {
-    private Map<Integer, List<Aresta>> adjacencias;
-    
+    Map<Integer, List<Aresta>> adjacencias;
+
     @SuppressWarnings("unused")
     private int numVertices;
 
     public Grafo() {
+        /* 
+         * adjacencias: mapeia um vértice para uma lista de arestas que estão conectadas a ele.
+         * numVertices: número de vértices no grafo.
+         */
         adjacencias = new HashMap<>();
         numVertices = 0;
     }
 
     public void inserirVertice(int id) {
         if (!adjacencias.containsKey(id)) {
-            adjacencias.put(id, new ArrayList<>());
+            adjacencias.put(id, new ArrayList<>()); // Cria uma nova lista de adjacências para o vértice.
             numVertices++;
         }
     }
 
     public void inserirAresta(int origem, int destino, int peso) {
         adjacencias.get(origem).add(new Aresta(destino, peso));
-        adjacencias.get(destino).add(new Aresta(origem, peso)); // Como o grafo é não direcionado
+        adjacencias.get(destino).add(new Aresta(origem, peso));
     }
 
     public void removerVertice(int id) {
@@ -31,10 +35,65 @@ class Grafo {
         numVertices--;
     }
 
+    /* 
+     * Remove a aresta entre os vértices origem e destino.
+     * removeIf: remove todos os elementos da lista que satisfazem a condição especificada.
+     */
     public void removerAresta(int origem, int destino) {
-        adjacencias.get(origem).removeIf(aresta -> aresta.destino == destino);
+        adjacencias.get(origem).removeIf(aresta -> aresta.destino == destino); 
         adjacencias.get(destino).removeIf(aresta -> aresta.destino == origem);
     }
 
-    // Outros métodos para as funcionalidades restantes...
+    public String buscaLargura(int verticeInicial) {
+        /* 
+         * BFS (Busca em Largura) é um algoritmo de busca em grafos que começa pelo vértice raiz e 
+         * explora todos os vértices vizinhos.
+         * Depois, para cada um desses vértices mais próximos, explora os seus vértices vizinhos inexplorados e assim por diante.
+         * Set<Integer> visitados: conjunto de vértices visitados
+         * Queue<Integer> fila: fila de vértices a serem visitados (primeiro a entrar, primeiro a sair)
+         * StringBuilder sb: string que armazenará a saída do BFS
+         * linkedList: implementação de uma fila em Java, implementa uma lista que permite a inserção e 
+         * remoção de elementos em ambas as extremidades da lista
+         */
+        Set<Integer> verticiesVisitados = new HashSet<>();
+        Queue<Integer> fila = new LinkedList<>();
+
+        /* 
+         * Adiciona o vértice inicial na fila e marca como visitado.
+         */
+        fila.add(verticeInicial);
+        verticiesVisitados.add(verticeInicial);
+        
+        StringBuilder valoresConcatenado = new StringBuilder("Busca em Largura: ");
+
+        // System.out.println("Busca em Largura: ");
+        while (!fila.isEmpty()) {
+            /* 
+             * Poll remove o primeiro elemento da fila e o retorna.
+             * O vértice atual é o primeiro da fila.
+             * Imprime o vértice atual.
+             */
+            int verticeAtual = fila.poll();
+            // System.out.print(verticeAtual + " - ");
+            valoresConcatenado.append(verticeAtual);
+            valoresConcatenado.append(" - ");
+
+
+            /* 
+             * Para cada vértice adjacente ao vértice atual, se ele não foi visitado,
+             * adiciona na fila e marca como visitado.
+             * O vértice adjacente é adicionado na fila e marcado como visitado.
+             * O loop continua até que todos os vértices tenham sido visitados.
+             */
+            for (int i = 0; i < adjacencias.get(verticeAtual).size(); i++) {
+                Aresta aresta = adjacencias.get(verticeAtual).get(i);
+                if (!verticiesVisitados.contains(aresta.destino)) {
+                    verticiesVisitados.add(aresta.destino);
+                    fila.add(aresta.destino);
+                }
+            }
+        }
+        return valoresConcatenado.toString();
+    }
+
 }
